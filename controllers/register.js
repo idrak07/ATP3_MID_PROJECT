49b3ef19e5373f37ed83b6fb2fcf02ce1f86d646
likhon <56948181+LikhonSarker12@users.noreply.github.com>
2019-10-31 21:46:48 +0600

    1st

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var router=express.Router();
+
 router.get('/',(req,res)=>{
     res.render('register/index');
 });
@@ -13,6 +14,29 @@ router.get('/university',(req,res)=>{
 });
 
 router.get('/organization',(req,res)=>{
-    res.render('register/organization');
+        res.render('register/organization');
+});
+router.post('/organization', function(request, response){
+
+	var user = {
+        organizationName: request.body.Organizationname,
+        organizationCode:request.body.Organizationcode,
+        address: request.body.Organizationaddress,
+        email:request.body.Emailaddress,
+        contact:request.body.Organizationcontact,
+		username: request.body.username,
+		password: request.body.password,
+		
+	};
+
+	userModel.insert(user, function(status){
+		
+		if(status){
+			response.redirect('/user/userlist');
+		}else{
+			response.redirect('/user/adduser');
+		}
+	});
+	
 });
 module.exports = router;
