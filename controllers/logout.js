ed41495ae7f6a64ad9389773850819e86545b967
likhon <56948181+LikhonSarker12@users.noreply.github.com>
2019-11-03 09:11:17 +0600

    4th

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
 router.get('/', function(request, response){
-
-	//request.session.un = null;
 	response.clearCookie('username');
 	response.redirect('/login');
 });
-
 module.exports = router;
--- models/organization-model.js
+++ models/organization-model.js
@@ -4,10 +4,11 @@ module.exports = {
 
 	getById: function(id, callback){
 
-			var sql = "select * from organization where id="+id;
-			db.getResults(sql, function(result){
+			var sql = "select * from organization where ID="+id;
+			db.getResults(sql,[], function(result){
 			
 				if(result.length > 0 ){
+					console.log('getid',result[0]);
 					callback(result[0]);
 				}else{
 					callback([]);
@@ -18,9 +19,9 @@ module.exports = {
 	getByUsername: function(username, callback){
 		var sql = "select * from organization where username='"+username+"'";
 
-		db.getResults(sql, function(result){
+		db.getResults(sql, [],function(result){
 			if(result.length > 0 ){
-				callback(result[0]);
+				callback(result);
 			}else{
 				callback([]);
 			}
@@ -50,10 +51,10 @@ module.exports = {
 			}
 		});	
 	},
-	getAllContent: function(callback){
-		var sql = "select * from content";
+	getAllofferscholarship: function(callback){
+		var sql = "select * from offerscholarship";
 		
-		db.getResults(sql, function(results){
+		db.getResults(sql,[], function(results){
 			
 			if(results.length > 0){
 				callback(results);
@@ -62,31 +63,69 @@ module.exports = {
 			}
 		});	
 	},
-	insertOrg: function(user, callback){
-
-		var sql ="insert into organization (orName,orCode,address,email,contact,username,passward) values('"+user.organizationName+"','"+user.organizationCode+"','"+user.address+"','"+user.email+"','"+user.contact+"','"+user.username+"', '"+user.password+"')";
-		db.execute(sql, function(status){
+	getAlluniversity: function(callback){
+		var sql = "select * from university";
+		
+		db.getResults(sql,[], function(results){
+			console.log('university',results);
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback([]);
+			}
+		});	
+	},
+	insertOrg: function(user,callback){
+			 
+		var sql="INSERT INTO `organization`(`orName`, `orCode`, `address`, `email`, `contact`,`username`, `password`,`status`) VALUES (?,?,?,?,?,?,?,?)";
+		//var sql ="insert into organization (orName,orCode,address,email,contact,username,passward) values('"+user.organizationName+"','"+user.organizationCode+"','"+user.address+"','"+user.email+"','"+user.contact+"','"+user.username+"', '"+user.password+"')";
+		db.execute(sql,[user.organizationName,user.organizationCode,user.address,user.email,user.contact,user.username,user.password,3], function(status){
 			callback(status);
 		});
 		
 	},
-	insertLogin: function(user, callback){
-
-		var sql ="insert into userlogin (userid,password) values('"+user.username+"', '"+user.password+"')";
-		db.execute(sql, function(status){
+	insertofferscholarship: function(user,callback){
+			 
+		var sql="INSERT INTO `offerscholarship`(`orName`,`university`, `program`, `number`, `scholarship`, `date`) VALUES (?,?,?,?,?,?)";
+		//var sql ="insert into organization (orName,orCode,address,email,contact,username,passward) values('"+user.organizationName+"','"+user.organizationCode+"','"+user.address+"','"+user.email+"','"+user.contact+"','"+user.username+"', '"+user.password+"')";
+		db.execute(sql,[user.orgname,user.university,user.program,user.number,user.scholarship,user.date], function(status){
 			callback(status);
 		});
+		
+	},
+	insertOeganizationLogin: function(user, callback){
+		var sql1="INSERT INTO `organization`(`orName`, `orCode`, `address`, `email`, `contact`,`username`, `password`,`status`) VALUES (?,?,?,?,?,?,?,?)";
+		var sql2= "INSERT INTO `userlogin`(`userid`, `password`, `status`) VALUES (?,?,?)";
+		db.execute(sql1,[user.organizationName,user.organizationCode,user.address,user.email,user.contact,user.username,user.password,3], function(status){
+			if(status)
+			{
+				db.execute(sql2,[user.username,user.password,3], function(status){
+					callback(status);
+				})
+			}
+			
+		});
+		
+		//var sql1111 ="insert into userlogin (userid,password) values('"+user.username+"', '"+user.password+"')";
+		;
 		
 	},
 	update: function(user, callback){
-		var sql ="update organization set username='"+ user.username+"', password='"+user.password+"' where id="+user.id;
-		
-		console.log(sql);
-
-		db.execute(sql, function(status){
-			callback(status);
+		var sql ="update organization set orName=?,address=?,email=?,contact=?,username=?, password=? where id=?";
+		var sql2 ="update userlogin set userid=?,password=?,status=? where userid=?";
+		//var sql2= "INSERT INTO `userlogin`(`userid`, `password`, `status`) VALUES (?,?,?)";
+		db.execute(sql, [user.orName,user.address,user.email,user.contact,user.username, user.password, user.id], function(status){
+			console.log('log',status);
+			if(status)
+			{
+				db.execute(sql2,[user.username,user.password,3,user.uid], function(status){
+					callback(status);
+				})
+			}
+			
 		});
 	},
+	
 	delete: function(id, callback){
 		var sql = "delete from organization where id="+id;
 		db.execute(sql, function(status){
